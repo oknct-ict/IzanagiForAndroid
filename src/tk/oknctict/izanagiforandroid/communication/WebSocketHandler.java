57ec566e6e5da2d0b67ba0a925a0c099f493913d
Daichi Teruya <tmarusa83@gmail.com>
2014-08-13 00:10:32 +0900

    サンプルのコピペを済ませました
    Websocketを試しに使うためのサンプルコードを写してみました。

--- src/tk/oknctict/izanagiforandroid/communication/WebSocketHandler.java
+++ src/tk/oknctict/izanagiforandroid/communication/WebSocketHandler.java
@@ -3,10 +3,21 @@ package tk.oknctict.izanagiforandroid.communication;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import tk.oknctict.izanagiforandroid.MainActivity;
 import android.os.Build;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 public class WebSocketHandler {
+	private static final String TAG = "WebSocketHandler";
+	
 	private static URI mUri = null;
+	private static WebSocketClient mClient = null;
+	private static Handler mHandler = null;
 	
 	/**
 	 * ê⁄ë±êÊÇÃURIÇÃÉZÉbÉ^
@@ -37,6 +48,39 @@ public class WebSocketHandler {
 			java.lang.System.setProperty("java.net.preferIPv6Addresses", "false");
 			java.lang.System.setProperty("java.net.preferIPv4Stack", "true");
 		}
+		
+		mHandler = new Handler();
+		mClient = new WebSocketClient(mUri){
+			@Override
+			public void onOpen(ServerHandshake handshake){
+				Log.d(TAG, "onOpen");
+			}
+			
+			@Override
+			public void onMessage(final String message){
+				Log.d(TAG, "onMessage");
+				Log.d(TAG, "Message:" + message);
+				mHandler.post(new Runnable(){
+					@Override
+					public void run(){
+						//TODO: implements show toast
+					}
+				});
+			}
+			
+			@Override
+			public void onError(Exception ex){
+				Log.d(TAG, "onError");
+				ex.printStackTrace();
+			}
+			
+			@Override
+			public void onClose(int code, String reason, boolean remote){
+				Log.d(TAG, "onClose");
+			}
+		};
+		
+		mClient.connect();
 		
 		return (0);
 	}
