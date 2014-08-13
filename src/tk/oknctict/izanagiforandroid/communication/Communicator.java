d05da8e09e2f15f03fca2a3d0e288595c2583157
Daichi Teruya <tmarusa83@gmail.com>
2014-08-13 17:42:52 +0900

    Communicatorコンストラクタを追記

--- src/tk/oknctict/izanagiforandroid/communication/Communicator.java
+++ src/tk/oknctict/izanagiforandroid/communication/Communicator.java
@@ -1,9 +1,19 @@
 package tk.oknctict.izanagiforandroid.communication;
 
+import java.net.URISyntaxException;
+
+import tk.oknctict.izanagiforandroid.communication.WebSocketHandlerSingleton;
+
 /**
  * Izanagi for Androidに必要な通信を行うクラス
  * @author marusa
  */
 public class Communicator {
+	private WebSocketHandlerSingleton wsHandler;
 	
+	public Communicator(String uri) throws URISyntaxException{
+		wsHandler = WebSocketHandlerSingleton.getInstance();
+		
+		wsHandler.setUri(uri);
+	}
 }
