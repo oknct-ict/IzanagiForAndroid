0b8a9ac029176a7b7b637bd4157a1b008ce5be4a
Daichi Teruya <tmarusa83@gmail.com>
2014-08-15 23:47:07 +0900

    UI部分とロジック部分の関連付けを行いました

--- src/tk/oknctict/izanagiforandroid/LoginActivity.java
+++ src/tk/oknctict/izanagiforandroid/LoginActivity.java
@@ -1,7 +1,11 @@
 package tk.oknctict.izanagiforandroid;
 
+import tk.oknctict.izanagiforandroid.R.id;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class LoginActivity extends Activity {
@@ -9,5 +13,13 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		Button login_button = (Button)findViewById(id.login_login_button);
+		login_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(LoginActivity.this, "login", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
