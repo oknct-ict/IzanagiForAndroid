15bed83ca759679f702572e93cc37053ba28030b
Daichi Teruya <tmarusa83@gmail.com>
2014-08-17 22:02:48 +0900

    ログインボタンとロジックを追記しました

--- src/tk/oknctict/izanagiforandroid/LoginActivity.java
+++ src/tk/oknctict/izanagiforandroid/LoginActivity.java
@@ -1,7 +1,15 @@
 package tk.oknctict.izanagiforandroid;
 
+import java.net.URISyntaxException;
+
+import tk.oknctict.izanagiforandroid.R.id;
+import tk.oknctict.izanagiforandroid.communication.Communicator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends Activity {
@@ -9,5 +17,28 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		Button login_button = (Button)findViewById(id.login_login_button);
+		login_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(LoginActivity.this, "login", Toast.LENGTH_SHORT).show();
+				
+				EditText usernameBox = (EditText)findViewById(R.id.login_username);
+				EditText passwdBox = (EditText)findViewById(R.id.login_passwd);
+				
+				String username = usernameBox.getText().toString();
+				String passwd = passwdBox.getText().toString();
+				
+				try {
+					Communicator communicator = new Communicator();
+					//communicator.establishConnection();
+					communicator.login(username, passwd);
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		});
 	}
 }
