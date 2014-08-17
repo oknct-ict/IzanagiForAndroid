aca3f8e4d3850ba17ed25cd78bb35609c92c999e
Daichi Teruya <tmarusa83@gmail.com>
2014-08-17 20:51:14 +0900

    ログインリクエストをPostするようにしました

--- src/tk/oknctict/izanagiforandroid/LoginActivity.java
+++ src/tk/oknctict/izanagiforandroid/LoginActivity.java
@@ -1,10 +1,14 @@
 package tk.oknctict.izanagiforandroid;
 
+import java.net.URISyntaxException;
+
 import tk.oknctict.izanagiforandroid.R.id;
+import tk.oknctict.izanagiforandroid.communication.Communicator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -19,6 +23,21 @@ public class LoginActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Toast.makeText(LoginActivity.this, "login", Toast.LENGTH_SHORT).show();
+				
+				EditText usernameBox = (EditText)findViewById(R.id.login_username);
+				EditText passwdBox = (EditText)findViewById(R.id.login_passwd);
+				
+				String username = usernameBox.getText().toString();
+				String passwd = passwdBox.getText().toString();
+				
+				try {
+					Communicator communicator = new Communicator();
+					communicator.establishConnection();
+					communicator.login(username, passwd);
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+				}
+				
 			}
 		});
 	}
