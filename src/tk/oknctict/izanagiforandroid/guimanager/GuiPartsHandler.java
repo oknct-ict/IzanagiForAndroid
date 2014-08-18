df75a89191b70e595428bd7d248b8c500e276d80
Daichi Teruya <tmarusa83@gmail.com>
2014-08-19 08:56:08 +0900

    パーツIDの設定が行えるようになりました

--- src/tk/oknctict/izanagiforandroid/guimanager/GuiPartsHandler.java
+++ src/tk/oknctict/izanagiforandroid/guimanager/GuiPartsHandler.java
@@ -7,6 +7,35 @@ import java.util.HashMap;
  * @author marusa
  */
 public class GuiPartsHandler {
+	private int mPartsType;
+	
+	/**
+	 * コンストラクタ
+	 * <pre>
+	 * 指定したタイプのパーツを保持します。  
+	 * </pre>
+	 * @param partsType
+	 * @throws UndefinedPartsTypeException 
+	 */
+	public GuiPartsHandler(int partsType) throws UndefinedPartsTypeException{
+		/* 存在しないパーツタイプのチェック */
+		if (partsType <= 0 || PARTS_TYPE_COUNT_PLUSONE <= partsType){
+			throw new UndefinedPartsTypeException();
+		}
+		
+		mPartsType = partsType;
+	}
+	public static final int PARTS_TYPE_BUTTON = 1;
+	public static final int PARTS_TYPE_TEXTVIEW = 2;
+	public static final int PARTS_TYPE_TEXTBOX = 3;
+	public static final int PARTS_TYPE_IMAGEVIEW = 4;
+	public static final int PARTS_TYPE_SHAPE = 5;
+	public static final int PARTS_TYPE_COUNT_PLUSONE = 6;
+	
+	public int getPartsType(){
+		return (mPartsType);
+	}
+	
 	/* Inner Classes */
 	/**
 	 * GUIパーツのイベントリスナを保持するクラスです。
@@ -18,5 +47,14 @@ public class GuiPartsHandler {
 		public void addMap(String type, String functionId){
 			listenerMap.put(type, functionId);
 		}
+	}
+	
+	/* exceptions */
+	public class UndefinedPartsTypeException extends Exception {
+		public UndefinedPartsTypeException(){
+			super("This partsType is not defined.");
+		}
+		
+		private static final long serialVersionUID = 5536758626328426886L;
 	}
 }
