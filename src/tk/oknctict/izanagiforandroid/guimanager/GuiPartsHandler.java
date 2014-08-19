16fe6e5147668899682b6f8ef036761471116c22
Daichi Teruya <tmarusa83@gmail.com>
2014-08-19 09:16:31 +0900

    オブジェクトの生成コードを書きました

--- src/tk/oknctict/izanagiforandroid/guimanager/GuiPartsHandler.java
+++ src/tk/oknctict/izanagiforandroid/guimanager/GuiPartsHandler.java
@@ -2,11 +2,18 @@ package tk.oknctict.izanagiforandroid.guimanager;
 
 import java.util.HashMap;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * GUI部品そのものと部品の操作のインターフェイスを持つクラスです
  * @author marusa
  */
 public class GuiPartsHandler {
+	private View mView;
 	private int mPartsType;
 	
 	/**
@@ -24,10 +31,31 @@ public class GuiPartsHandler {
 		}
 		
 		mPartsType = partsType;
+		switch (mPartsType){
+		case PARTS_TYPE_BUTTON:
+			mView = (View)(new Button(null));
+			break;
+			
+		case PARTS_TYPE_TEXTVIEW:
+			mView = (View)(new TextView(null));
+			break;
+			
+		case PARTS_TYPE_EDITTEXT:
+			mView = (View)(new EditText(null));
+			break;
+			
+		case PARTS_TYPE_IMAGEVIEW:
+			mView = (View)(new ImageView(null));
+			break;
+			
+		case PARTS_TYPE_SHAPE:
+			//TODO: Shapeの定義しましょう
+			break;
+		}
 	}
 	public static final int PARTS_TYPE_BUTTON = 1;
 	public static final int PARTS_TYPE_TEXTVIEW = 2;
-	public static final int PARTS_TYPE_TEXTBOX = 3;
+	public static final int PARTS_TYPE_EDITTEXT = 3;
 	public static final int PARTS_TYPE_IMAGEVIEW = 4;
 	public static final int PARTS_TYPE_SHAPE = 5;
 	public static final int PARTS_TYPE_COUNT_PLUSONE = 6;
