39e08ba7fdc52f8bd772c5e27d2d3b1c9e045810
Daichi Teruya <tmarusa83@gmail.com>
2014-08-21 15:52:03 +0900

    アクティビティーを追加

---
+++ src/tk/oknctict/izanagiforandroid/NoConnectionActivity.java
@@ -0,0 +1,13 @@
+package tk.oknctict.izanagiforandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NoConnectionActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+}
